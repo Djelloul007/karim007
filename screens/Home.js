5e0e319e3ee9fd20a2ceacd479f563d28da9c7b3
Djelloul007 <belarbi@gmx.net>
2022-03-20 18:15:06 +0100

    add country list

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { View, Button, Text, StyleSheet } from "react-native";
-
+const names = ['Deutschland', 'France', 'Algerien']
 const Home = ({ navigation }) => {
   return (
     <View style={styles.center}>
       <Text>This is the home screen</Text>
+      {names.map(name => <Button title={name} onPress={() => navigation.navigate("About")} />)}
       <Button
         title="Go to About Screen"
-        onPress={() => navigation.navigate("About")} 
+        onPress={() => navigation.navigate("About")}
       />
 
-    <Button
-            title="Deutschland"
-            onPress={() => navigation.navigate("Deutschland")} 
-        />
+      <Button
+        title="Deutschland"
+        onPress={() => navigation.navigate("Deutschland")}
+      />
     </View>
   );
 };
